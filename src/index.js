1a87d794101f34ea50c994fc06b96352cc16af2e
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-26 18:28:08 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-import _ from "lodash";
 import "./style.css";
 
-function component() {
-  const element = document.createElement("div");
+const LISTFORM = document.querySelector(".list");
+const LITASKCON = document.createElement("li");
+const INPUT = document.createElement("input");
+const SPAN = document.createElement("span");
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+const TASKSARR = [
+  {
+    description: "Clean the living room",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "Wash the dishes",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "Tidy my desktop",
+    completed: false,
+    index: 2,
+  },
+];
 
-  return element;
-}
+const DISPLAYTASKS = () => {
+  TASKSARR.forEach((task) => {
+    const LI_TASK_ITEM = LITASKCON.cloneNode(true);
+    const CHECKBOX = INPUT.cloneNode(true);
+    const INPUT_TEXT_CONTAINER = INPUT.cloneNode(true);
+    const ICON = SPAN.cloneNode(true);
+    console.log(ICON);
 
-document.body.appendChild(component());
+    LI_TASK_ITEM.classList.add("list__task__item", "icons");
+    CHECKBOX.setAttribute("type", "checkbox");
+    CHECKBOX.classList.add("list__checkbox");
+    INPUT_TEXT_CONTAINER.setAttribute("type", "text");
+    INPUT_TEXT_CONTAINER.setAttribute("name", "task");
+    INPUT_TEXT_CONTAINER.setAttribute("value", task.description);
+    INPUT_TEXT_CONTAINER.classList.add("list__input__text");
+    ICON.classList.add("icons");
+    ICON.innerText = "&#x022EE;";
+
+    LI_TASK_ITEM.append(CHECKBOX, INPUT_TEXT_CONTAINER, ICON);
+    LISTFORM.appendChild(LI_TASK_ITEM);
+  });
+};
+
+DISPLAYTASKS();
