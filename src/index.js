74480fa7142978b7eff7166f2b27396730083609
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-26 19:26:46 -0300

    trying linters

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import "./style.css";
+import './style.css';
 
-const LISTFORM = document.querySelector(".list");
-const LITASKCON = document.createElement("li");
-const INPUT = document.createElement("input");
-const SPAN = document.createElement("span");
+const LISTFORM = document.querySelector('.list');
+const LITASKCON = document.createElement('li');
+const INPUT = document.createElement('input');
+const SPAN = document.createElement('span');
 
 const TASKSARR = [
   {
-    description: "Clean the living room",
+    description: 'Clean the living room',
     completed: false,
     index: 0,
   },
   {
-    description: "Wash the dishes",
+    description: 'Wash the dishes',
     completed: false,
     index: 1,
   },
   {
-    description: "Tidy my desktop",
+    description: 'Tidy my desktop',
     completed: false,
     index: 2,
   },
@@ -30,15 +30,15 @@ const DISPLAYTASKS = () => {
     const INPUT_TEXT_CONTAINER = INPUT.cloneNode(true);
     const ICON = SPAN.cloneNode(true);
 
-    LI_TASK_ITEM.classList.add("list__task__item");
-    CHECKBOX.setAttribute("type", "checkbox");
-    CHECKBOX.classList.add("list__checkbox");
-    INPUT_TEXT_CONTAINER.setAttribute("type", "text");
-    INPUT_TEXT_CONTAINER.setAttribute("name", "task");
-    INPUT_TEXT_CONTAINER.setAttribute("value", task.description);
-    INPUT_TEXT_CONTAINER.classList.add("list__input__text");
-    ICON.classList.add("icons");
-    ICON.innerHTML = "&#x022EE;";
+    LI_TASK_ITEM.classList.add('list__task__item');
+    CHECKBOX.setAttribute('type', 'checkbox');
+    CHECKBOX.classList.add('list__checkbox');
+    INPUT_TEXT_CONTAINER.setAttribute('type', 'text');
+    INPUT_TEXT_CONTAINER.setAttribute('name', 'task');
+    INPUT_TEXT_CONTAINER.setAttribute('value', task.description);
+    INPUT_TEXT_CONTAINER.classList.add('list__input__text');
+    ICON.classList.add('icons');
+    ICON.innerHTML = '&#x022EE;';
 
     LI_TASK_ITEM.append(CHECKBOX, INPUT_TEXT_CONTAINER, ICON);
     LISTFORM.appendChild(LI_TASK_ITEM);
