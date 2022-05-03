a0b84acb929c3acbc5494d45997bbea5af87150c
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-03 09:07:19 -0300

    Just fix red line through

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './style.css';
-import TasksBluePrint from './tasks-functionality.js';
+import "./style.css";
+import TasksBluePrint from "./tasks-functionality.js";
 
 const copyTasksBluePrint = new TasksBluePrint();
 
-const tasksContainers = document.getElementById('tasks-div');
+const tasksContainers = document.getElementById("tasks-div");
 
 const createDynamicTasks = () => {
   tasksContainers.innerHTML = `
@@ -29,11 +29,11 @@ const createDynamicTasks = () => {
       .sort((a, b) => a.index - b.index)
       .map(
         (
-          task,
+          task
         ) => `<div class='entry-line task-item-line'><div class='inside-list-container'><input class='task-status' data-id='${
           task.index
         }' type='checkbox' ${
-          task.completed ? 'checked' : ''
+          task.completed ? "checked" : ""
         }><p class='task-description' id="desc${
           task.index
         }" contenteditable='true' data-tid='${task.index}'>${
@@ -41,66 +41,70 @@ const createDynamicTasks = () => {
         }</p>
           </div>
           <button type='button' data-taskid='${task.index}' id='delete${
-  task.index
-}' class='list-btn remove-btn'><i class='fa-solid fa-trash-can'></i></button>
-          </div>`,
+          task.index
+        }' class='list-btn remove-btn'><i class='fa-solid fa-trash-can'></i></button>
+          </div>`
       )
-      .join('');
-    tasksContainers.innerHTML
-      += '<div class="entry-line clear-task-line"><button id="remove-completed-btn" type="button">Clear all completed</button></div>';
+      .join("");
+    tasksContainers.innerHTML +=
+      '<div class="entry-line clear-task-line"><button id="remove-completed-btn" type="button">Clear all completed</button></div>';
 
-    const taskDescriptions = document.querySelectorAll('.task-description');
-    const deleteBtns = document.querySelectorAll('.remove-btn');
+    const taskDescriptions = document.querySelectorAll(".task-description");
+    const deleteBtns = document.querySelectorAll(".remove-btn");
 
     deleteBtns.forEach((btn) => {
-      btn.addEventListener('click', () => {
+      btn.addEventListener("click", () => {
         copyTasksBluePrint.remove(btn.dataset.taskid);
         createDynamicTasks();
       });
     });
 
-    const taskStatusesInfo = document.querySelectorAll('.task-status');
+    const taskStatusesInfo = document.querySelectorAll(".task-status");
 
     taskStatusesInfo.forEach((status) => {
-      status.addEventListener('change', () => {
+      status.addEventListener("change", () => {
         copyTasksBluePrint.changeStatus(status.dataset.id, status.checked);
         taskDescriptions.forEach((desc) => {
-          if (desc.dataset.tid === status.dataset.id) {
-            desc.classList.add('task-line-through');
-            desc.style = 'color: red;';
+          if (
+            parseInt(desc.dataset.tid, 10) === parseInt(status.dataset.id, 10)
+          ) {
+            desc.classList.add("task-line-through");
+            desc.style = "color: red;";
           }
         });
         createDynamicTasks();
       });
     });
 
-    const deleteCompletedBtn = document.getElementById('remove-completed-btn');
+    const deleteCompletedBtn = document.getElementById("remove-completed-btn");
 
-    deleteCompletedBtn.addEventListener('click', () => {
+    deleteCompletedBtn.addEventListener("click", () => {
       copyTasksBluePrint.removeCompletedTasks();
       createDynamicTasks();
     });
 
     taskDescriptions.forEach((taskObjItem) => {
-      taskObjItem.addEventListener('keyup', () => {
+      taskObjItem.addEventListener("keyup", () => {
         copyTasksBluePrint.update(
           taskObjItem.dataset.tid,
-          taskObjItem.textContent,
+          taskObjItem.textContent
         );
       });
-      taskObjItem.addEventListener('focus', () => {
-        taskObjItem.parentElement.parentElement.style = 'background-color: rgb(255 234 99)';
+      taskObjItem.addEventListener("focus", () => {
+        taskObjItem.parentElement.parentElement.style =
+          "background-color: rgb(255 234 99)";
       });
-      taskObjItem.addEventListener('blur', () => {
-        taskObjItem.parentElement.parentElement.style = 'background-color: #fff';
+      taskObjItem.addEventListener("blur", () => {
+        taskObjItem.parentElement.parentElement.style =
+          "background-color: #fff";
       });
     });
   }
 
-  const submitTaskForm = document.getElementById('add-task-form');
-  const newTask = document.querySelector('.input-new-task');
+  const submitTaskForm = document.getElementById("add-task-form");
+  const newTask = document.querySelector(".input-new-task");
 
-  submitTaskForm.addEventListener('submit', (event) => {
+  submitTaskForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const upcomingtask = {
       description: newTask.value,
@@ -108,7 +112,7 @@ const createDynamicTasks = () => {
       index: copyTasksBluePrint.funcSize() + 1,
     };
     copyTasksBluePrint.add(upcomingtask);
-    newTask.value = '';
+    newTask.value = "";
     createDynamicTasks();
   });
 };
